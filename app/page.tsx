caac6646598bcb105293c408b536259f08755b08
Ishan <ishanm1603@gmail.com>
2025-05-28 12:43:14 +0530

    add CTA section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import React from 'react'
 const Page = () => {
   return (
     <main>
-    <h1 className='text-2xl underline'>Popular Companions</h1>
+    <h1 className='text-2xl'>Popular Companions</h1>
 
     <section className='home-section'>
       <CompanionCard 
