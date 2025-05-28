ac472dbab2e835da690f7375b549cf080d3b94cd
Ishan <ishanm1603@gmail.com>
2025-05-28 17:23:50 +0530

    add clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
@@ -20,10 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${bricolage.variable} antialiased`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
+        <body className={`${bricolage.variable} antialiased`}>
+          <ClerkProvider appearance={{variables: {colorPrimary: '#fe5933'}}}>
+            <Navbar />
+            {children}
+          </ClerkProvider>
+        </body>
+      </html>
   );
 }
