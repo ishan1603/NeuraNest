d9b0172a8f16c9f202912cd5bc1f27795a5026d6
Ishan <ishanm1603@gmail.com>
2025-05-28 10:42:16 +0530

    add companion cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,45 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+    <h1 className='text-2xl underline'>Popular Companions</h1>
+
+    <section className='home-section'>
+      <CompanionCard 
+        id="123"
+        name="Neura the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e"
+      />
+      <CompanionCard 
+      id="456"
+      name="Countsy the Number Wizard"
+      topic="Integrals & Derivatives"
+      subject="science"
+      duration={30}
+      color="#e5d0ff"
+      />
+      <CompanionCard 
+      id="789"
+      name="Verba the vocabulary builder"
+      topic="Language Learning"
+      subject="English Literature"
+      duration={30}
+      color="#BDE7FF"
+      />
+    </section>
+
+    <section className='home-section'>
+      <CompanionsList />
+      <CTA />
+    </section>
+    </main>
   )
 }
 
