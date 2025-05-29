3deb12a59a33e276a4f11031c2624befe4cfc171
Ishan <ishanm1603@gmail.com>
2025-05-29 19:52:53 +0530

    add Vapi AI

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,17 +1,15 @@
-import CompanionCard from "@/components/CompanionCard";
-import SearchInput from "@/components/SearchInput";
-import SubjectFilter from "@/components/SubjectFilter";
-import { getAllCompanions } from "@/lib/actions/companion.actions";
-import { getSubjectColor } from "@/lib/utils";
+import CompanionCard from '@/components/CompanionCard';
+import SearchInput from '@/components/SearchInput';
+import SubjectFilter from '@/components/SubjectFilter';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+import { getSubjectColor } from '@/lib/utils';
 
-
-const CompanionsLibrary = async ({searchParams}: SearchParams) => {
-
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
   const filters = await searchParams;
   const subject = filters.subject ? filters.subject : '';
   const topic = filters.topic ? filters.topic : '';
 
-  const companions = await getAllCompanions({subject, topic})
+  const companions = await getAllCompanions({ subject, topic });
 
   return (
     <main>
@@ -23,12 +21,16 @@ const CompanionsLibrary = async ({searchParams}: SearchParams) => {
         </div>
       </section>
       <section className="companions-grid">
-        {companions.map((companion) => (
-          <CompanionCard key={companion.id} {...companion} color={getSubjectColor(companion.subject)} />
+        {companions.map(companion => (
+          <CompanionCard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
         ))}
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default CompanionsLibrary
+export default CompanionsLibrary;
